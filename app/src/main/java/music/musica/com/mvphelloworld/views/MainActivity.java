2658a24a631183bc00ce4894e4e43892c0ea94aa
skyways <amoskrr@gmail.com>
2018-08-21 07:31:36 +0300

    first mvp model structure

---
+++ app/src/main/java/music/musica/com/mvphelloworld/views/MainActivity.java
@@ -0,0 +1,15 @@
+package music.musica.com.mvphelloworld.views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import music.musica.com.mvphelloworld.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
